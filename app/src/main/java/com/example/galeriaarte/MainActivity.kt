ef55a52995b4644417745fb53f36a5082471b423
heitortraspadini <heitorg.traspadini@gmail.com>
2025-03-24 10:08:02 -0300

    Initial commit

---
+++ app/src/main/java/com/example/galeriaarte/MainActivity.kt
@@ -0,0 +1,122 @@
+package com.example.galeriaarte
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.galeriaarte.ui.theme.GaleriaArteTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge() // Permite que o conteúdo ocupe a tela toda, incluindo as bordas
+        setContent {
+            GaleriaArteTheme {
+                GaleriaLayout()
+            }
+        }
+    }
+}
+
+data class PecaDeArte(
+    @DrawableRes val imageRes: Int,
+    @StringRes val title: Int,
+    @StringRes val artist: Int
+)
+
+val pecasDeArte = listOf(
+    PecaDeArte(R.drawable.ponterionitero, R.string.tituloPonte, R.string.autorPonte),
+    PecaDeArte(R.drawable.espelho, R.string.tituloEspelho, R.string.autorEspelho),
+    PecaDeArte(R.drawable.mintefuck, R.string.tituloFuji, R.string.autorFuji)
+)
+
+@Composable
+fun GaleriaLayout(modifier: Modifier = Modifier) {
+    var etapaAtual by remember { mutableIntStateOf(0) }
+
+    val artPiece = pecasDeArte[etapaAtual] // Obter a peça de arte com base na etapa atual
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = 30.dp)
+            .padding(top = 75.dp, bottom = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        // Box para a imagem
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(430.dp)
+                .border(
+                    width = 30.dp,
+                    color = Color(0xFFFFFFFF),
+                    shape = RectangleShape
+                )
+                .shadow(20.dp)
+        ) {
+            Image(
+                modifier = modifier
+                    .fillMaxSize(),
+                painter = painterResource(artPiece.imageRes),
+                contentDescription = null
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = stringResource(artPiece.title), fontSize = 20.sp)
+            Text(text = stringResource(artPiece.artist), fontSize = 12.sp)
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Row(
+            modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Button(onClick = {
+                etapaAtual = if (etapaAtual > 0) etapaAtual - 1 else pecasDeArte.size - 1
+            }) {
+                Text("Imagem anterior")
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Button(onClick = {
+                if (etapaAtual < pecasDeArte.size - 1) etapaAtual += 1
+                else etapaAtual = 0
+            }) {
+                Text("Próxima imagem")
+            }
+        }
+    }
+}
+
+
